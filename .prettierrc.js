7908b25dbf3385f1e7593f43d2a90ed83767f59e
Naren Bellala <naren.bellala@amido.com>
2020-01-05 23:27:44 +0000

    feat:Added basic set of config for monorepo

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    semi: true,
+    trailingComma: 'all',
+    printWidth: 120,
+    singleQuote: true,
+    tabWidth: 4,
+};
--- webapp/pages/index.tsx
+++ webapp/pages/index.tsx
@@ -1,3 +1,3 @@
 const Home = () => <div>Welcome to Stacks-react app!</div>;
-
+// fetch('google.com')
 export default Home;
