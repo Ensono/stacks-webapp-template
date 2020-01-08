fab1f7e86a3cd581a72feb188debfc9ebe45a3d4
estherthetesteramido <esther.lloyd@amido.com>
2020-01-08 09:11:08 +0000

    fix(unit): Update the test name to be more readable

--- packages/webapp/__tests__/index.test.tsx
+++ packages/webapp/__tests__/index.test.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import {render} from "@testing-library/react"
 import App from "../pages/index"
 
-test('renders a page of tag div with text', () => {
+test('Renders a page of tag type <div> with text', () => {
   const { getByText } = render(<App />);
 
   expect(getByText('Welcome to Stacks-react app!')).not.toBeNull();
