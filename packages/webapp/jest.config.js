b9c03582860eb35a8a27af305064da49c24889f7
Naren Bellala <naren.bellala@amido.com>
2020-01-10 16:08:42 +0000

    fix: test commit for merge from master

--- packages/webapp/jest.config.js
+++ packages/webapp/jest.config.js
@@ -1,3 +1,27 @@
 module.exports = {
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  testPathIgnorePatterns: [
+    '<rootDir>/.next/',
+    '<rootDir>/node_modules/',
+    '<rootDir>/__tests__/coverage/',
+  ],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/*.{js,jsx,tsx}',
+    '!**/*config.{js,json}',
+    '!**/coverage/**',
+    '!**/node_modules/**',
+    '!**/.next/**',
+    '!**/next-env.d.ts',
+    '!**/dist/**',
+  ],
+  coverageDirectory: '<rootDir>./__tests__/coverage/',
+  coverageThreshold: {
+    global: {
+      statements: 100,
+      branches: 100,
+      functions: 100,
+      lines: 100,
+    },
+  },
+  verbose: true,
 }
--- packages/webapp/server/index.ts
+++ packages/webapp/server/index.ts
@@ -1,26 +1,27 @@
-import next from 'next';
-import express from 'express';
+import next from 'next'
+import express from 'express'
 
-const port = parseInt(process.env.PORT || '3000', 10);
-const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
-const handle = app.getRequestHandler();
-app.prepare().then(
-  () => {
-    const server = express();
+const port = parseInt(process.env.PORT || '3000', 10)
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({dev})
+const handle = app.getRequestHandler()
 
-    server.get('/', (req, res) => app.render(req, res, '/', req.query));
+export default app
+  .prepare()
+  .then(() => {
+    const server = express()
 
-    server.all('*', (req, res) => handle(req, res));
+    server.get('/', (req, res) => app.render(req, res, '/', req.query))
 
-    server.listen(port, (err) => {
-      if (err) throw err;
+    server.all('*', (req, res) => handle(req, res))
+
+    server.listen(port, err => {
+      if (err) throw err
       // eslint-disable-next-line no-console
-      console.log(`> Ready on http://localhost:${port}`);
-    });
-  },
-  (e) => {
-    // eslint-disable-next-line no-console
-    console.log('server initialization failed:', e);
-  },
-);
+      console.log(`> Ready on http://localhost:${port}`)
+    })
+  })
+  .catch((ex: any) => {
+    console.error(ex.stack)
+    process.exit(1)
+  })
