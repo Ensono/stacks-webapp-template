8731e8254e6db1c76c2d058db901334fb9be30ef
estherthetesteramido <esther.lloyd@amido.com>
2020-01-22 14:21:19 +0000

    fix(index): Fix spelling

--- packages/template-cli/templates/src/ssr/pages/index.tsx
+++ packages/template-cli/templates/src/ssr/pages/index.tsx
@@ -1,4 +1,4 @@
 import * as React from 'react'
 
-const Home = () => <div>Welcome to Stacks-react app! your current envirnment is: {process.env.NODE_ENV}</div>
+const Home = () => <div>Welcome to Stacks-react app! your current environment is: {process.env.NODE_ENV}</div>
 export default Home
