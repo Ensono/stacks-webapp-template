0151a2fed90f12ae0a2f88a802c56436ee18d3ad
estherthetesteramido <esther.lloyd@amido.com>
2020-01-27 11:46:02 +0000

    feat(jest): Configure jest for transpiling with Babel for Typescript

--- packages/template-cli/jest.config.js
+++ packages/template-cli/jest.config.js
@@ -1,29 +1,32 @@
 module.exports = {
-  testPathIgnorePatterns: [
-    '<rootDir>/.next/',
-    '<rootDir>/node_modules/',
-    '<rootDir>/__tests__/coverage/',
-  ],
-  testResultsProcessor: 'jest-sonar-reporter',
-  coverageReporters: ['lcov'],
-  collectCoverage: true,
-  collectCoverageFrom: [
-    '**/*.{js,jsx,tsx}',
-    '!**/*config.{js,json}',
-    '!**/coverage/**',
-    '!**/node_modules/**',
-    '!**/.next/**',
-    '!**/next-env.d.ts',
-    '!**/dist/**',
-  ],
-  coverageDirectory: '<rootDir>/coverage/',
-  coverageThreshold: {
-    global: {
-      statements: 100,
-      branches: 100,
-      functions: 100,
-      lines: 100,
+    roots: [`<rootDir>.`],
+    transform: {
+        '^.+.ts.?$': 'ts-jest'
     },
-  },
-  verbose: true,
+    testPathIgnorePatterns: [
+        '<rootDir>/node_modules/',
+        '<rootDir>/coverage/',
+        '<rootDir>/templates/'
+    ],
+    testResultsProcessor: 'jest-sonar-reporter',
+    coverageReporters: ['lcov'],
+    collectCoverage: true,
+    collectCoverageFrom: [
+        '**/*.{ts,tsx}',
+        '!**/*config.{js,json}',
+        '!**/coverage/**',
+        '!**/templates/**',
+        '!**/coverage/**',
+        '!**/node_modules/**'
+    ],
+    coverageDirectory: '<rootDir>./coverage/',
+    coverageThreshold: {
+        global: {
+            statements: 100,
+            branches: 100,
+            functions: 100,
+            lines: 100,
+        },
+    },
+    verbose: true,
 }
