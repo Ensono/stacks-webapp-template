fe713448f231c623e87d9ab9089c9c26c6742a41
estherthetesteramido <esther.lloyd@amido.com>
2020-02-05 17:12:58 +0000

    chore: Update the readme ensure the broker is a env variable

--- packages/template-cli/templates/src/ssr/pact/pactPublish.ts
+++ packages/template-cli/templates/src/ssr/pact/pactPublish.ts
@@ -3,7 +3,7 @@ import { provider } from '../pact/pactSetup'
 
 export const publisher = new Publisher({
     pactFilesOrDirs: [provider.opts.dir],
-    pactBroker: 'https://amido-stacks.pact.dius.com.au',
+    pactBroker: process.env['Test.PactBroker'] || process.env.PACT_BROKER,
     pactBrokerToken: process.env['Test.PactBearerToken'] || process.env.PACT_BEARER_TOKEN,
     consumerVersion: process.env.BUILD_NUMBER || 'unknown'
 })
