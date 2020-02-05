84dd681d709809711aba2afd7bb337924110808e
estherthetesteramido <esther.lloyd@amido.com>
2020-02-05 10:44:02 +0000

    fix: ensure only unit tests are run

--- packages/template-cli/templates/src/ssr/jest.config.js
+++ packages/template-cli/templates/src/ssr/jest.config.js
@@ -1,29 +1,30 @@
 module.exports = {
-  testPathIgnorePatterns: [
-    '<rootDir>/.next/',
-    '<rootDir>/node_modules/',
-    '<rootDir>/__tests__/coverage/',
-  ],
-  testResultsProcessor: 'jest-sonar-reporter',
-  coverageReporters: ['lcov'],
-  collectCoverage: true,
-  collectCoverageFrom: [
-    '**/*.{js,jsx,tsx}',
-    '!**/*config.{js,json}',
-    '!**/coverage/**',
-    '!**/node_modules/**',
-    '!**/.next/**',
-    '!**/next-env.d.ts',
-    '!**/dist/**',
-  ],
-  coverageDirectory: '<rootDir>/coverage/',
-  coverageThreshold: {
-    global: {
-      statements: 100,
-      branches: 100,
-      functions: 100,
-      lines: 100,
+    testPathIgnorePatterns: [
+        '<rootDir>/.next/',
+        '<rootDir>/node_modules/',
+        '<rootDir>/__tests__/coverage/',
+    ],
+    testMatch: ['**/*.test.(ts|tsx)'],
+    testResultsProcessor: 'jest-sonar-reporter',
+    coverageReporters: ['lcov'],
+    collectCoverage: true,
+    collectCoverageFrom: [
+        '**/*.{js,jsx,tsx}',
+        '!**/*config.{js,json}',
+        '!**/coverage/**',
+        '!**/node_modules/**',
+        '!**/.next/**',
+        '!**/next-env.d.ts',
+        '!**/dist/**',
+    ],
+    coverageDirectory: '<rootDir>/coverage/',
+    coverageThreshold: {
+        global: {
+            statements: 100,
+            branches: 100,
+            functions: 100,
+            lines: 100,
+        },
     },
-  },
-  verbose: true,
+    verbose: true,
 }
