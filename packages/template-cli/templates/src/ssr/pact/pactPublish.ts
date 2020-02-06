d40284bd0da8a0cd1f124243dc4e9e747d336098
estherthetesteramido <esther.lloyd@amido.com>
2020-02-06 17:59:50 +0000

    feat: Deploy the contracts to the Broker in pipleline

--- packages/template-cli/templates/src/ssr/pact/pactPublish.ts
+++ packages/template-cli/templates/src/ssr/pact/pactPublish.ts
@@ -6,7 +6,7 @@ export const publisher = new Publisher({
     pactBroker: process.env.PACT_BROKER,
     pactBrokerToken: process.env.PACT_BEARER_TOKEN,
     consumerVersion: process.env.BUILD_NUMBER || 'unknown',
-    tags: ['local']
+    tags: [process.env.SOURCE_BRANCH_NAME || 'local']
 })
 
 publisher.publish()
