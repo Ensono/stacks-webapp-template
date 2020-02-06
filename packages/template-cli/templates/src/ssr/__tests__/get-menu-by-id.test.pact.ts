c7935faadd47804f439a18c583be8c9484b300ed
estherthetesteramido <esther.lloyd@amido.com>
2020-02-06 15:51:11 +0000

    feat: Another test, matching the example laid out by the provider

--- packages/template-cli/templates/src/ssr/__tests__/get-menu-by-id.test.pact.ts
+++ packages/template-cli/templates/src/ssr/__tests__/get-menu-by-id.test.pact.ts
@@ -12,17 +12,10 @@ describe('Yumido Menu API', () => {
     menuService = new MenuService({ url, port: provider.opts.port })
 
     const EXPECTED_BODY = {
-        "pageSize": 1,
-        "pageNumber": 1,
-        "results": [
-            {
-                "id": "1234",
-                "restaurantId": "1234",
-                "name": "Breakfast Menu",
-                "description": "Eggs, Bread, Coffee and more",
-                "enabled": true
-            }
-        ]
+        "id": "d290f1ee-6c54-4b01-90e6-d701748f0851",
+        "name": "Burger Menu",
+        "description": "Cheese burger",
+        "enabled": true
     }
 
     afterEach(() => {
@@ -33,19 +26,18 @@ describe('Yumido Menu API', () => {
         beforeEach(() => {
             const interaction = new Interaction()
                 .given('An existing menu')
-                .uponReceiving('A request for all menus')
+                .uponReceiving('A request for a menu by ID')
                 .withRequest({
                     method: 'GET',
-                    path: '/v1/menu',
+                    path: '/v1/menu/7f993e28-b9b1-4ea7-830b-b30f9758db68',
                     headers: {
                         Accept: 'application/json',
-                    },
-                    query: 'pageSize=1&pageNumber=1'
+                    }
                 })
                 .willRespondWith({
                     status: 200,
                     headers: {
-                        'Content-Type': 'application/json',
+                        'Content-Type': 'application/json; charset=utf-8',
                     },
                     body: EXPECTED_BODY,
                 })
@@ -54,9 +46,9 @@ describe('Yumido Menu API', () => {
         })
 
         it('sends a request according to contract', () => {
-            return menuService.getMenu()
+            return menuService.getMenuById()
                 .then((response: any) => {
-                    expect(response.headers["content-type"]).toEqual("application/json")
+                    expect(response.headers["content-type"]).toEqual("application/json; charset=utf-8")
                     expect(response.status).toEqual(200)
                     expect(response.data).toEqual(EXPECTED_BODY)
             })
--- packages/template-cli/templates/src/ssr/server/apis/menuapi/mocks/menuService.ts
+++ packages/template-cli/templates/src/ssr/server/apis/menuapi/mocks/menuService.ts
@@ -22,4 +22,13 @@ export class MenuService {
             }
         })
     }
+
+    public getMenuById = (): AxiosPromise => {
+        return axios.request({
+            baseURL: `${this.url}:${this.port}`,
+            headers: { Accept: "application/json" },
+            method: "GET",
+            url: "/v1/menu/7f993e28-b9b1-4ea7-830b-b30f9758db68"
+        })
+    }
 }
