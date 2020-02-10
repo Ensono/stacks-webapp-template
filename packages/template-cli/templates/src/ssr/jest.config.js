1383f8c90a7895b972715acf30650b5e81691ba0
estherthetesteramido <esther.lloyd@amido.com>
2020-02-10 09:15:31 +0000

    chore: output test report in junit for Azure pipelines

--- packages/template-cli/templates/src/ssr/jest.config.js
+++ packages/template-cli/templates/src/ssr/jest.config.js
@@ -1,30 +1,34 @@
 module.exports = {
-  testPathIgnorePatterns: [
-    '<rootDir>/.next/',
-    '<rootDir>/node_modules/',
-    '<rootDir>/__tests__/coverage/',
-  ],
-  testMatch: ['**/*.test.(ts|tsx)'],
-  testResultsProcessor: 'jest-sonar-reporter',
-  coverageReporters: ['lcov'],
-  collectCoverage: true,
-  collectCoverageFrom: [
-    '**/*.{js,jsx,tsx}',
-    '!**/*config.{js,json}',
-    '!**/coverage/**',
-    '!**/node_modules/**',
-    '!**/.next/**',
-    '!**/next-env.d.ts',
-    '!**/dist/**',
-  ],
-  coverageDirectory: '<rootDir>/coverage/',
-  coverageThreshold: {
-    global: {
-      statements: 100,
-      branches: 100,
-      functions: 100,
-      lines: 100,
+    transform: {
+        '^.+.ts.?$': 'ts-jest',
     },
-  },
-  verbose: true,
+    testPathIgnorePatterns: [
+        '<rootDir>/.next/',
+        '<rootDir>/node_modules/',
+        '<rootDir>/__tests__/coverage/',
+    ],
+    testMatch: ['**/*.test.(ts|tsx)'],
+    testResultsProcessor: 'jest-sonar-reporter',
+    reporters: ['default', ['jest-junit', {outputName: 'junit-test-report.xml'}]],
+    coverageReporters: ['lcov'],
+    collectCoverage: true,
+    collectCoverageFrom: [
+        '**/*.{js,jsx,tsx}',
+        '!**/*config.{js,json}',
+        '!**/coverage/**',
+        '!**/node_modules/**',
+        '!**/.next/**',
+        '!**/next-env.d.ts',
+        '!**/dist/**',
+    ],
+    coverageDirectory: '<rootDir>/coverage/',
+    coverageThreshold: {
+        global: {
+            statements: 100,
+            branches: 100,
+            functions: 100,
+            lines: 100,
+        },
+    },
+    verbose: true,
 }
