30c0251c6d8271c201d0c75b50d3b9e97e5095c8
estherthetesteramido <esther.lloyd@amido.com>
2020-02-10 12:44:11 +0000

    fix: tsconfig for jest and next

--- packages/template-cli/jest.config.js
+++ packages/template-cli/jest.config.js
@@ -1,33 +1,34 @@
 module.exports = {
-  roots: [`<rootDir>.`],
-  transform: {
-    '^.+.ts.?$': 'ts-jest',
-  },
-  testPathIgnorePatterns: [
-    '<rootDir>/node_modules/',
-    '<rootDir>/coverage/',
-    '<rootDir>/templates/',
-  ],
-  testResultsProcessor: 'jest-sonar-reporter',
-  reporters: ['default', ['jest-junit', {outputName: 'junit-test-report.xml'}]],
-  coverageReporters: ['cobertura', 'lcov'],
-  collectCoverage: true,
-  collectCoverageFrom: [
-    '**/*.{ts,tsx}',
-    '!**/*config.{js,json}',
-    '!**/coverage/**',
-    '!**/templates/**',
-    '!**/coverage/**',
-    '!**/node_modules/**',
-  ],
-  coverageDirectory: '<rootDir>./coverage/',
-  coverageThreshold: {
-    global: {
-      statements: 0,
-      branches: 0,
-      functions: 0,
-      lines: 0,
+    roots: [`<rootDir>.`],
+    transform: {
+        '^.+.ts.?$': 'ts-jest',
     },
-  },
-  verbose: true,
+    testPathIgnorePatterns: [
+        '<rootDir>/node_modules/',
+        '<rootDir>/coverage/',
+        '<rootDir>/templates/',
+    ],
+    testMatch: ['**/*.test.(ts|tsx)'],
+    testResultsProcessor: 'jest-sonar-reporter',
+    reporters: ['default', ['jest-junit', {outputName: 'junit-test-report.xml'}]],
+    coverageReporters: ['cobertura', 'lcov'],
+    collectCoverage: true,
+    collectCoverageFrom: [
+        '**/*.{ts,tsx}',
+        '!**/*config.{js,json}',
+        '!**/coverage/**',
+        '!**/templates/**',
+        '!**/coverage/**',
+        '!**/node_modules/**',
+    ],
+    coverageDirectory: '<rootDir>./coverage/',
+    coverageThreshold: {
+        global: {
+            statements: 0,
+            branches: 0,
+            functions: 0,
+            lines: 0,
+        },
+    },
+    verbose: true,
 }
