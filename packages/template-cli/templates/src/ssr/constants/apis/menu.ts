3baf5131709f78103b9c0e2251e309996ecfe359
Dusan Nitschneider <dusannitschneider@AML0127.local>
2020-02-11 13:29:28 +0000

    fix: added preceeding slash to express routes

--- packages/template-cli/templates/src/ssr/constants/apis/menu.ts
+++ packages/template-cli/templates/src/ssr/constants/apis/menu.ts
@@ -6,31 +6,31 @@ class MenuApi implements Api {
   version = "v1";
   endpoints = {
     getMenuList: {
-      routeDefinition: 'menus/:id?', // this is how I expect it to be called
+      routeDefinition: '/menus/:id?', // this is how I expect it to be called
       getInternalURL: (id: number)=>`/menus/${id}`, // this is to build the URL internally for the FE
       getExternalURL: () => "menu", // this is the URL that will be called externally
       method: HTTPMethod.get
     },
     getMenu: {
-      routeDefinition: 'menu/:id',
+      routeDefinition: '/menu/:id',
       getInternalURL: (id: string)=>`/menu/${id}`,
       getExternalURL: (id: string) => `menu/${id}`,
       method: HTTPMethod.get
     },
     deleteMenu: {
-      routeDefinition: 'menu/:id',
+      routeDefinition: '/menu/:id',
       getInternalURL: (id: string)=>`/deletemenu/${id}`,
       getExternalURL: (id: string) => `menu/${id}`,
       method: HTTPMethod.delete
     },
     updateMenu: {
-      routeDefinition: 'menu/:id',
+      routeDefinition: '/menu/:id',
       getInternalURL: (id: string)=>`/updatemenu/${id}`,
       getExternalURL: (id: string) => `menu/${id}`,
       method: HTTPMethod.put
     },
     addMenu: {
-      routeDefinition: 'menu',
+      routeDefinition: '/menu',
       getInternalURL: ()=>'/addmenu',
       getExternalURL: () => `menu`,
       method: HTTPMethod.post
