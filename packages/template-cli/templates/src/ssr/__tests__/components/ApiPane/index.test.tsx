97146ebb0dc377228d827ac185013dffe9950e97
Naren Bellala <naren.bellala@amido.com>
2020-02-16 08:22:00 +0000

    feat: added initial snapshot tests

--- packages/template-cli/templates/src/ssr/__tests__/components/ApiPane/index.test.tsx
+++ packages/template-cli/templates/src/ssr/__tests__/components/ApiPane/index.test.tsx
@@ -1,8 +1,15 @@
-import * as React from 'react'
-import {render} from '@testing-library/react'
-import ApiPane from '../../../components/ApiPane'
+import '@testing-library/jest-dom/extend-expect';
+import { fireEvent, render } from '@testing-library/react';
+import * as React from 'react';
+import ApiPane from '../../../components/ApiPane';
 
 test('renders ApiPane snapshot', () => {
     const {asFragment} = render(<ApiPane />)
     expect(asFragment()).toMatchSnapshot()
 });
+
+test('fires api call on button click', () => {
+    const { getByText } = render(<ApiPane />)
+    expect(getByText('get')).toBeInTheDocument()
+    fireEvent.click(getByText('get'));
+})
