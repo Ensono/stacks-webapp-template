e30c62047c877bc0114d2266359082207a847fb2
Naren Bellala <naren.bellala@amido.com>
2020-02-17 12:04:31 +0000

    fix: removed deleted files

--- packages/template-cli/templates/src/config/environment-variables.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  NODE_ENV: process.env.NODE_ENV,
-  PORT: process.env.PORT,
-  APP_BASE_URL: process.env.APP_BASE_URL,
-  APP_BASE_PATH: process.env.APP_BASE_PATH || '',
-  MENU_API_URL: process.env.MENU_API_URL,
-  LOG_LEVEL: process.env.LOG_LEVEL,
-  POD_NAME: process.env.POD_NAME,
-}
