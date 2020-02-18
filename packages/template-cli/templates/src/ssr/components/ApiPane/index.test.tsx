25872208617f1a7241a4fa255f636e706df634a7
Naren Bellala <naren.bellala@amido.com>
2020-02-18 16:08:38 +0000

    fix: broken unit test by importing the runtime con

--- packages/template-cli/templates/src/ssr/components/ApiPane/index.test.tsx
+++ packages/template-cli/templates/src/ssr/components/ApiPane/index.test.tsx
@@ -1,10 +1,11 @@
 // import "@testing-library/jest-dom/extend-expect"
 import { fireEvent, render, screen, waitForElement } from '@testing-library/react'
-import axios from 'axios'
+import * as axios from 'axios'
 import * as React from 'react'
 import { toMatchDiffSnapshot } from 'snapshot-diff'
 import ApiPane from '.'
 // jest.mock('axios', () => ({get: jest.fn()}))
+jest.mock("next/config", () => () => ({publicRuntimeConfig: {}}))
 jest.mock('axios', () =>
   jest.fn(() =>
     Promise.resolve({
