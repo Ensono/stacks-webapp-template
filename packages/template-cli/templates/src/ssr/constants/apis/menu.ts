d137760ed604e6ab99747a021e98987127cc107f
naren-bellala <40792222+naren-bellala@users.noreply.github.com>
2020-02-18 15:15:11 +0000

    feat: basic snapshots of api pane component (#41)
    * feat: added initial snapshot to the api component
    
    * fix: auto formatting commit
    
    * feat: added initial snapshot tests
    
    * feat: added diff snapshoting test
    
    * feat: added tests for api integration
    
    * fix: updated the tests locations
    
    * fix: typescript expect assertion fixed by importing right package
    
    * fix: cleaned up unused debug logger files
    
    * fix: build failure due to package lock missing
    
    * fix: declared jest extended in global
    
    * fix: prettier config update for ts files
    
    * fix: extended assertion in each test file
    
    * fix: added global typescript config extend-expect

--- packages/template-cli/templates/src/ssr/constants/apis/menu.ts
+++ packages/template-cli/templates/src/ssr/constants/apis/menu.ts
@@ -1,43 +1,43 @@
-import { Api, HTTPMethod, buildExport } from ".";
-import conf from "../../config"
+import {Api, HTTPMethod, buildExport} from '.'
+import conf from '../../config'
 
 class MenuApi implements Api {
-    baseURL = conf.MENU_API_URL;
-    internalBasePath = conf.APP_BASE_PATH;
-    version = "v1";
-    endpoints = {
-        getMenuList: {
-            routeDefinition: '/menu', // this is how I expect it to be called
-            getInternalURL: () => `menu`, // this is to build the URL internally for the FE
-            getExternalURL: () => "menu", // this is the URL that will be called externally
-            method: HTTPMethod.get
-        },
-        getMenu: {
-            routeDefinition: '/menu/:id',
-            getInternalURL: (id: string) => `menu/${id}`,
-            getExternalURL: (id: string) => `menu/${id}`,
-            method: HTTPMethod.get
-        },
-        deleteMenu: {
-            routeDefinition: '/menu/:id',
-            getInternalURL: (id: string) => `deletemenu/${id}`,
-            getExternalURL: (id: string) => `menu/${id}`,
-            method: HTTPMethod.delete
-        },
-        updateMenu: {
-            routeDefinition: '/menu/:id',
-            getInternalURL: (id: string) => `updatemenu/${id}`,
-            getExternalURL: (id: string) => `menu/${id}`,
-            method: HTTPMethod.put
-        },
-        addMenu: {
-            routeDefinition: '/menu',
-            getInternalURL: () => 'addmenu',
-            getExternalURL: () => `menu`,
-            method: HTTPMethod.post
-        }
-    };
+  baseURL = conf.APP_BASE_URL
+  internalBasePath = conf.APP_BASE_PATH
+  version = 'v1'
+  endpoints = {
+    getMenuList: {
+      routeDefinition: '/menu', // this is how I expect it to be called
+      getInternalURL: () => `menu`, // this is to build the URL internally for the FE
+      getExternalURL: () => 'menu', // this is the URL that will be called externally
+      method: HTTPMethod.get,
+    },
+    getMenu: {
+      routeDefinition: '/menu/:id',
+      getInternalURL: (id: string) => `menu/${id}`,
+      getExternalURL: (id: string) => `menu/${id}`,
+      method: HTTPMethod.get,
+    },
+    deleteMenu: {
+      routeDefinition: '/menu/:id',
+      getInternalURL: (id: string) => `deletemenu/${id}`,
+      getExternalURL: (id: string) => `menu/${id}`,
+      method: HTTPMethod.delete,
+    },
+    updateMenu: {
+      routeDefinition: '/menu/:id',
+      getInternalURL: (id: string) => `updatemenu/${id}`,
+      getExternalURL: (id: string) => `menu/${id}`,
+      method: HTTPMethod.put,
+    },
+    addMenu: {
+      routeDefinition: '/menu',
+      getInternalURL: () => 'addmenu',
+      getExternalURL: () => `menu`,
+      method: HTTPMethod.post,
+    },
+  }
 }
-
-const menuApi = new MenuApi();
-export default (endpoint: keyof typeof menuApi.endpoints) => buildExport(menuApi, endpoint)
+const menuApi = new MenuApi()
+export default (endpoint: keyof typeof menuApi.endpoints) =>
+  buildExport(menuApi, endpoint)
