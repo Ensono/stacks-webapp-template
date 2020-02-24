e1e0d21d59f30c5e4cd4e89e5969d2a5112d8f5a
estherthetesteramido <esther.lloyd@amido.com>
2020-02-24 14:10:52 +0000

    fix: next server config bundler

--- packages/template-cli/templates/src/ssr/next.config.js
+++ packages/template-cli/templates/src/ssr/next.config.js
@@ -17,6 +17,8 @@ module.exports = {
         return config
     },
     assetPrefix: process.env.APP_BASE_PATH || "",
+    // Only include tsx files for serving, excluding the *.spec.ts files for Cypress
+    pageExtensions: ['tsx'],
     target: "server",
     serverRuntimeConfig: {
         // Will only be available on the server side
