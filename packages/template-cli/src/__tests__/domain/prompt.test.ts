dd2d9f31f692beb768f513a3408f634c138c096c
dnitsch <nitschneiderd@gmail.com>
2020-03-04 17:03:23 +0000

    feat: extend CLI logic to implement smart folder selection (#68)
    * feat: implement CLI extension for smart folder mapping
    updated tests
    updated docs for contributors
    
    * chore: linter adjustment
    
    * fix: addresssed comments
    
    * fix: changed title in markdown

--- packages/template-cli/src/__tests__/domain/prompt.test.ts
+++ packages/template-cli/src/__tests__/domain/prompt.test.ts
@@ -17,18 +17,20 @@ jest.mock('../../domain/selectors')
 describe("prompt class tests", () => {
 
     describe("Positive assertions when run from config", () => {
-        let cliResult: ExitMessage
-        beforeEach(async () => {
-            cliResult = await prompt.runCli('test', [resolve(__dirname, 'sample.bootstrap-config.json')])
-        });
         it("should return an object with a code and message", async () => {
+            FlowSelector.option_ssr_aks_azuredevops = jest.fn().mockImplementationOnce(() => {
+                return Promise.resolve({code: 0, message: ""})
+            });
+
+            let cliResult: ExitMessage = await prompt.runCli('test', [resolve(__dirname, 'sample.bootstrap-config.json')])
+
             expect(cliResult).toHaveProperty("code")
             expect(cliResult).toHaveProperty("message")
             expect(cliResult.code).toBe(0)
         })
     })
     describe("Negative assertions", () => {
-        it("should return a code of 127 when excpetion is thrown", async () => {
+        it("should return a response with an error code and an excpetion inside error", async () => {
             FlowSelector.option_ssr_aks_azuredevops = jest.fn().mockImplementationOnce(() => {
                 throw {code: 127, message: new Error("Something weird happened")};
             });
