8e379c35c4d88359c9b73fcd4dfdd35ca4967ecf
Naren Bellala <naren.bellala@amido.com>
2020-03-18 20:26:55 +0000

    feat: click label to return home

--- packages/template-cli/templates/src/ssr/components/Header/index.tsx
+++ packages/template-cli/templates/src/ssr/components/Header/index.tsx
@@ -1,18 +1,29 @@
 import React from "react"
-import { AppBar, Fab, Toolbar, Tooltip, Typography } from "@material-ui/core"
+import {AppBar, Fab, Toolbar, Tooltip, Typography} from "@material-ui/core"
 import AddIcon from "@material-ui/icons/Add"
-import { PrefixedLink as Link } from "components"
-import { useRouter } from "next/router"
+import {PrefixedLink as Link} from "components"
+import {useRouter} from "next/router"
+import styled from "@emotion/styled"
+
+const StyledLink = styled.a`
+    text-decoration: none;
+    cursor: pointer;
+`
 
 const title: string = `Yumido`
 
 export const Header = () => {
-    const isCreatePage = useRouter()?.pathname.split("/").pop() === "create"
+    const isCreatePage =
+        useRouter()
+            ?.pathname.split("/")
+            .pop() === "create"
     return (
         <AppBar position="fixed" color="secondary">
             <Toolbar>
                 <Typography variant="h2" style={{margin: "0 auto"}}>
-                    {title}
+                    <Link href="/">
+                        <StyledLink>{title}</StyledLink>
+                    </Link>
                 </Typography>
                 {!isCreatePage && (
                     <Link href="/create">
