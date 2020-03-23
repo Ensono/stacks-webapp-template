3e9dac420a119d07238ff3a1858f68d16b8be744
naren-bellala <40792222+naren-bellala@users.noreply.github.com>
2020-03-23 14:46:54 +0000

    feat: ui improvements (#94)
    * fix: 1482-added the full url for favicon
    
    * fix: 1482-removed port
    
    * feat: click label to return home
    
    * test: added test for post api
    
    * test: added axe tests for components
    
    * test: added tests for header
    
    * test: added for form
    
    * test: e2e tests added
    
    * fix: broken tests in ci
    
    * test: 1323-pact test cases added for post api
    
    Co-authored-by: estherthetesteramido <esther.lloyd@amido.com>

--- packages/template-cli/templates/src/ssr/__tests__/pact/mocks/menuService.ts
+++ packages/template-cli/templates/src/ssr/__tests__/pact/mocks/menuService.ts
@@ -31,4 +31,19 @@ export class MenuService {
             url: "/v1/menu/7f993e28-b9b1-4ea7-830b-b30f9758db68"
         })
     }
+    
+    public addMenu = () => {
+        return axios.request({
+            baseURL: `${this.url}:${this.port}`,
+            headers: { Accept: "application/json" },
+            method: "POST",
+            url: "/v1/menu/",
+            data: JSON.stringify({
+                name: "",
+                description: "",
+                enabled: false,
+                tenantId: "",
+            })
+        })
+    }
 }
