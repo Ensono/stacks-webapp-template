65503265afbcc511bb72c534e16893fb6e6d4a05
Esther Lloyd <47354603+estherthetesteramido@users.noreply.github.com>
2020-04-07 08:40:03 +0100

    Fix: pact test reporting (#110)
    * fix: publishing reports
    
    * chore: fail if pact validation fails
    
    * fix: reference wrappper script
    
    * chore: update package-lock

--- packages/template-cli/templates/src/ssr/__tests__/pact/utils/pactSetup.ts
+++ packages/template-cli/templates/src/ssr/__tests__/pact/utils/pactSetup.ts
@@ -5,8 +5,8 @@ const port = 8080;
 
 export const provider = new Pact({
     port,
-    log: resolve(process.cwd(), '__tests__', 'logs', 'mockserver-integration.log'),
-    dir: resolve(process.cwd(), '__tests__', 'pacts'),
+    log: resolve(process.cwd(), '__tests__', 'pact', 'logs', 'mockserver-integration.log'),
+    dir: resolve(process.cwd(), '__tests__', 'pact', 'pacts'),
     spec: 2,
     cors: true,
     pactfileWriteMode: 'update',
