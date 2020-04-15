dd9efae162d7704bf935e69f41b7afcf880cb26a
Naren Bellala <naren.bellala@amido.com>
2020-04-15 16:43:46 +0100

    test: snapshot component

---
+++ packages/template-cli/templates/src/ssr/components/Search/index.test.tsx
@@ -0,0 +1,10 @@
+import {render, screen, waitForElement} from "@testing-library/react"
+import React from "react"
+import { Search } from "."
+
+test("renders ApiPane snapshot", () => {
+    const {asFragment} = render(
+        <Search getSearchResults />,
+    )
+    expect(asFragment()).toMatchSnapshot()
+})
