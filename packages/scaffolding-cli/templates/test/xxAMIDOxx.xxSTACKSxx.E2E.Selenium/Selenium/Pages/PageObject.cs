f4618d0bf3f9448999c5277b3dc4b079c15a9301
Esther Lloyd <47354603+estherthetesteramido@users.noreply.github.com>
2020-04-24 11:33:21 +0100

    .NET selenium template framework (#130)
    * feat: move template solution to repo
    
    * chore: linting in json
    
    * feat: whole test
    
    * feat: page slash component object model

---
+++ packages/scaffolding-cli/templates/test/xxAMIDOxx.xxSTACKSxx.E2E.Selenium/Selenium/Pages/PageObject.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace xxAMIDOxx.xxSTACKSxx.E2E.Selenium.Selenium.Pages
+{
+  public abstract class PageObject
+  {
+    protected readonly IWebDriver WebDriver;
+    protected readonly WebDriverWait wait;
+
+    protected PageObject(IWebDriver webDriver)
+    {
+      WebDriver = webDriver;
+      wait = new WebDriverWait(WebDriver, TimeSpan.FromSeconds(5));
+    }
+
+    public void Select(By locator)
+    {
+      SelectElementWhenClickable(locator);
+    }
+
+    public void Click(By locator)
+    {
+      ClickElementWhenVisible(locator);
+    }
+
+    public bool Displayed(By locator)
+    {
+      WaitForElementToBeVisible(locator);
+      return true;
+    }
+
+    public void TypeText(By locator, string text)
+    {
+      SendKeysToElementWhenVisible(locator, text);
+    }
+
+    public string Text(By locator)
+    {
+      return GetElementTextWhenVisible(locator);
+    }
+
+    protected void WaitForElementToBeVisible(By locator, int timeoutSeconds = 5)
+    {
+      wait.Timeout = TimeSpan.FromSeconds(timeoutSeconds);
+
+      try
+      {
+        wait.Until(WebDriver => WebDriver.FindElement(locator).Displayed);
+      }
+      catch (Exception e)
+      {
+        throw new TimeoutException(e.InnerException?.ToString() ?? e.ToString());
+      }
+    }
+
+    protected void WaitForElementToBeClickable(By locator, int timeoutSeconds = 5)
+    {
+      wait.Timeout = TimeSpan.FromSeconds(timeoutSeconds);
+
+      try
+      {
+        wait.Until(WebDriver => WebDriver.FindElement(locator).Enabled);
+      }
+      catch (Exception e)
+      {
+        throw new TimeoutException(e.InnerException?.ToString() ?? e.ToString());
+      }
+    }
+
+    protected void ClickElementWhenVisible(By locator, int timeoutSeconds = 5)
+    {
+      WaitForElementToBeVisible(locator, timeoutSeconds);
+      WebDriver.FindElement(locator).Click();
+    }
+
+    protected void SelectElementWhenClickable(By locator, int timeoutSeconds = 5)
+    {
+      WaitForElementToBeClickable(locator, timeoutSeconds);
+      WebDriver.FindElement(locator).Click();
+    }
+
+    protected void SendKeysToElementWhenVisible(By locator, string text, int timeoutSeconds = 5)
+    {
+      WaitForElementToBeVisible(locator, timeoutSeconds);
+      WebDriver.FindElement(locator).Clear();
+      WebDriver.FindElement(locator).SendKeys(text);
+    }
+
+    protected string GetElementTextWhenVisible(By locator, int timeoutSeconds = 5)
+    {
+      WaitForElementToBeVisible(locator, timeoutSeconds);
+      return WebDriver.FindElement(locator).Text;
+    }
+  }
+}
---
+++ packages/scaffolding-cli/templates/test/xxAMIDOxx.xxSTACKSxx.E2E.Selenium/Tests/Stories/GetMenuExampleTests.cs
@@ -0,0 +1,34 @@
+using TestStack.BDDfy;
+using xxAMIDOxx.xxSTACKSxx.E2E.Selenium.Tests.Fixtures;
+using xxAMIDOxx.xxSTACKSxx.E2E.Selenium.Tests.Steps;
+using Xunit;
+
+namespace xxAMIDOxx.xxSTACKSxx.E2E.Selenium.Tests.Stories
+{
+  [Story(
+      AsA = "an exisiting restaurant on Yumido",
+      IWant = "to create a new menu",
+      SoThat = "hungry customers can view my offerings")]
+
+  public class GetMenuExampleTests : IClassFixture<BaseSetup>
+  {
+    private readonly BaseSetup fixtures;
+    private readonly CreateMenu steps;
+
+    public GetMenuExampleTests(BaseSetup fixture)
+    {
+      fixtures = fixture;
+      steps = new CreateMenu(fixture.SeleniumWrapper);
+    }
+
+    [Fact]
+    public void CreateNewMenu()
+    {
+      this.Given(step => fixtures.Home())
+          .Given(step => steps.ARestaurantWithMenus())
+          .When(step => steps.ICreateANewMenu())
+          .Then(step => steps.TheResturantShouldHaveANewMenu())
+          .BDDfy();
+    }
+  }
+}
