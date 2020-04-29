f01d48c92f964fb8588e90e650b7da2af0eed2f2
estherthetesteramido <esther.lloyd@amido.com>
2020-04-29 16:51:09 +0100

    chore: remote debug

--- packages/scaffolding-cli/templates/test/xxAMIDOxx.xxSTACKSxx.E2E.Selenium/Selenium/Factory/WebDriverFactory.cs
+++ packages/scaffolding-cli/templates/test/xxAMIDOxx.xxSTACKSxx.E2E.Selenium/Selenium/Factory/WebDriverFactory.cs
@@ -2,22 +2,26 @@
 using OpenQA.Selenium.Chrome;
 using System.IO;
 using System.Reflection;
+using System.Collections.Generic;
 
 namespace xxAMIDOxx.xxSTACKSxx.E2E.Selenium.Selenium.Factory
 {
-  public class WebDriverFactory
-  {
-    public static IWebDriver GetWebDriver()
+    public class WebDriverFactory
     {
-      // Default: chromedriver
-      // chromedriver executable is copied to the output folder from the package source folder
-      // with the build process.
-      // e.g. xxAMIDOxx.xxSTACKSxx.E2E.Selenium/bin/Debug/netcoreapp3.1/chromedriver
+        public static IWebDriver GetWebDriver()
+        {
+            // Default: chromedriver headless
+            // chromedriver executable is copied to the output folder from the package source folder
+            // with the build process.
+            // e.g. xxAMIDOxx.xxSTACKSxx.E2E.Selenium/bin/Debug/netcoreapp3.1/chromedriver
 
-      string outPutDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-      string chromeDriverPath = Path.GetFullPath(outPutDirectory);
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArguments(new List<string>() { "no-sandbox", "headless", "disable-gpu", "remote-debugging-port=1559" });
 
-      return new ChromeDriver(chromeDriverPath);
+            string outPutDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string chromeDriverPath = Path.GetFullPath(outPutDirectory);
+
+            return new ChromeDriver(chromeDriverPath, chromeOptions);
+        }
     }
-  }
 }
