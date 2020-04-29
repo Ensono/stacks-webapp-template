fa7e27afd7271887ef43457bd1e7372ee3f48840
estherthetesteramido <esther.lloyd@amido.com>
2020-04-29 15:56:30 +0100

    feat: update chromedriver

--- packages/scaffolding-cli/templates/test/xxAMIDOxx.xxSTACKSxx.E2E.Selenium/Selenium/Factory/WebDriverFactory.cs
+++ packages/scaffolding-cli/templates/test/xxAMIDOxx.xxSTACKSxx.E2E.Selenium/Selenium/Factory/WebDriverFactory.cs
@@ -1,5 +1,7 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System.IO;
+using System.Reflection;
 
 namespace xxAMIDOxx.xxSTACKSxx.E2E.Selenium.Selenium.Factory
 {
@@ -7,7 +9,15 @@ namespace xxAMIDOxx.xxSTACKSxx.E2E.Selenium.Selenium.Factory
   {
     public static IWebDriver GetWebDriver()
     {
-      return new ChromeDriver();
+      // Default: chromedriver
+      // chromedriver executable is copied to the output folder from the package source folder
+      // with the build process.
+      // e.g. xxAMIDOxx.xxSTACKSxx.E2E.Selenium/bin/Debug/netcoreapp3.1/chromedriver
+
+      string outPutDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+      string chromeDriverPath = Path.GetFullPath(outPutDirectory);
+
+      return new ChromeDriver(chromeDriverPath);
     }
   }
 }
