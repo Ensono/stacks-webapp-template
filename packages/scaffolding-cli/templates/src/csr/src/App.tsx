b0d32bf760d38b9bd196d687d43dd9e536bf27c2
Naren Bellala <naren.bellala@amido.com>
2020-04-30 22:54:04 +0100

    feat: updated with MUI and Emotion

--- packages/scaffolding-cli/templates/src/csr/src/App.tsx
+++ packages/scaffolding-cli/templates/src/csr/src/App.tsx
@@ -1,25 +1,16 @@
+import {ThemeProvider as MuiThemeProvider} from "@material-ui/core/styles"
 import React from "react"
-import logo from "./logo.svg"
 import "./App.css"
+import Header from "./components/Header"
+import theme from "./constants/theme"
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <MuiThemeProvider theme={theme}>
+            <div className="App">
+                <Header />
+            </div>
+        </MuiThemeProvider>
     )
 }
 
