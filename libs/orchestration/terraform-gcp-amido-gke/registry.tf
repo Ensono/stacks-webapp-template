10c4eee7cbb1287359cb81772859258d7e93bd85
Dusan Nitschneider <nitschneiderd@gmail.com>
2020-05-12 10:39:13 +0100

    fix: ensure shared infra gets spun up

--- libs/orchestration/terraform-gcp-amido-gke/registry.tf
+++ libs/orchestration/terraform-gcp-amido-gke/registry.tf
@@ -1,9 +1,11 @@
+# NB: GCP creates a registry automagically upon pushing the first image into it
 # # TODO: allow location override
-resource "google_container_registry" "default" {
-  provider = google-beta
-  project  = "${var.resource_namer}-registry"
-  location = "EU"
-}
+
+# resource "google_container_registry" "default" {
+#   provider = google-beta
+#   project  = "${var.resource_namer}-registry"
+#   location = "EU"
+# }
 
 # resource "google_storage_bucket" "default_gcr" {
 #   name          = "${var.resource_namer}-registry"
--- libs/orchestration/terraform-gcp-amido-gke/routing.tf
+++ libs/orchestration/terraform-gcp-amido-gke/routing.tf
@@ -8,72 +8,73 @@ resource "google_dns_managed_zone" "default" {
   # labels = merge(var.tags, map("ManagedBy", "Amido"))
 }
 
-resource "google_compute_managed_ssl_certificate" "default" {
-  provider = google-beta
+# # will need to move this to the application module
+# resource "google_compute_managed_ssl_certificate" "default" {
+#   provider = google-beta
 
-  name = var.resource_namer
-  type = "MANAGED"
-  managed {
-    domains = ["*.${var.dns_zone}."]
-  }
-}
+#   name = var.resource_namer
+#   type = "MANAGED"
+#   managed {
+#     domains = ["*.${var.dns_zone}."]
+#   }
+# }
 
-resource "google_compute_target_https_proxy" "default" {
-  provider = google-beta
+# resource "google_compute_target_https_proxy" "default" {
+#   provider = google-beta
 
-  name             = var.resource_namer
-  url_map          = google_compute_url_map.default.self_link
-  ssl_certificates = [google_compute_managed_ssl_certificate.default.self_link]
-}
+#   name             = var.resource_namer
+#   url_map          = google_compute_url_map.default.self_link
+#   ssl_certificates = [google_compute_managed_ssl_certificate.default.self_link]
+# }
 
-resource "google_compute_url_map" "default" {
-  provider = google-beta
+# resource "google_compute_url_map" "default" {
+#   provider = google-beta
 
-  name        = var.resource_namer
-  description = "a description"
+#   name        = var.resource_namer
+#   description = "a description"
 
-  default_service = google_compute_backend_service.default.self_link
+#   default_service = google_compute_backend_service.default.self_link
 
-  host_rule {
-    hosts        = [var.dns_zone]
-    path_matcher = "allpaths"
-  }
+#   host_rule {
+#     hosts        = [var.dns_zone]
+#     path_matcher = "allpaths"
+#   }
 
-  path_matcher {
-    name            = "allpaths"
-    default_service = google_compute_backend_service.default.self_link
+#   path_matcher {
+#     name            = "allpaths"
+#     default_service = google_compute_backend_service.default.self_link
 
-    path_rule {
-      paths   = ["/*"]
-      service = google_compute_backend_service.default.self_link
-    }
-  }
-}
+#     path_rule {
+#       paths   = ["/*"]
+#       service = google_compute_backend_service.default.self_link
+#     }
+#   }
+# }
 
-resource "google_compute_backend_service" "default" {
-  provider = google-beta
+# resource "google_compute_backend_service" "default" {
+#   provider = google-beta
 
-  name        = var.resource_namer
-  port_name   = "http"
-  protocol    = "HTTP"
-  timeout_sec = 10
+#   name        = var.resource_namer
+#   port_name   = "http"
+#   protocol    = "HTTP"
+#   timeout_sec = 10
 
-  health_checks = [google_compute_http_health_check.default.self_link]
-}
+#   health_checks = [google_compute_http_health_check.default.self_link]
+# }
 
-resource "google_compute_http_health_check" "default" {
-  provider = google-beta
+# resource "google_compute_http_health_check" "default" {
+#   provider = google-beta
 
-  name               = "http-health-check"
-  request_path       = "/"
-  check_interval_sec = 1
-  timeout_sec        = 1
-}
+#   name               = "http-health-check"
+#   request_path       = "/"
+#   check_interval_sec = 1
+#   timeout_sec        = 1
+# }
 
-resource "google_compute_global_forwarding_rule" "default" {
-  provider = google-beta
+# resource "google_compute_global_forwarding_rule" "default" {
+#   provider = google-beta
 
-  name       = var.resource_namer
-  target     = google_compute_target_https_proxy.default.self_link
-  port_range = 443
-}
+#   name       = var.resource_namer
+#   target     = google_compute_target_https_proxy.default.self_link
+#   port_range = 443
+# }
