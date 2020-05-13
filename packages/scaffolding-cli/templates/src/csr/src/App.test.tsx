bdba7d2a6f3becd2c7013100ddd4677f74c8abfc
Naren Bellala <naren.bellala@amido.com>
2020-05-13 10:57:48 +0100

    chore: dummy

--- packages/scaffolding-cli/templates/src/csr/src/App.test.tsx
+++ packages/scaffolding-cli/templates/src/csr/src/App.test.tsx
@@ -1,5 +1,5 @@
-import React from "react"
 import {render} from "@testing-library/react"
+import React from "react"
 import App from "./App"
 
 test("With React Testing Library page renders tag type <App> with text", () => {
