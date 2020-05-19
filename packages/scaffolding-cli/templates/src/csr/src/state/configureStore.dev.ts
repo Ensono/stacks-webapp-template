7d5a482817b9825ec21094e56e346c381af3e373
Naren Bellala <naren.bellala@amido.com>
2020-05-19 12:09:01 +0100

    chore: added redux dev tools to CSR

--- packages/scaffolding-cli/templates/src/csr/src/state/configureStore.dev.ts
+++ packages/scaffolding-cli/templates/src/csr/src/state/configureStore.dev.ts
@@ -1,12 +1,24 @@
-import {applyMiddleware, createStore, Store} from "redux"
+import {applyMiddleware, createStore, Store, compose} from "redux"
 import {ApplicationState, rootReducer, rootSaga} from "./ducks/index"
 import sagaMiddleware from "./middlewares/sagas"
+
+const hasDevTools =
+    process.env.NODE_ENV === "development" &&
+    // eslint-disable-next-line
+    (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+
+const composeEnhancers = hasDevTools
+    ? // eslint-disable-next-line
+      (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : compose
 
 export default function configureStore(
     initialState: ApplicationState,
 ): Store<ApplicationState> {
-    const middlewares = applyMiddleware(sagaMiddleware) // Create Store
-    const store = createStore(rootReducer, initialState, middlewares)
+    const middlewares = applyMiddleware(sagaMiddleware)
+    const enhancers = [middlewares]
+    const composedEnhancers = composeEnhancers(...enhancers)
+    const store = createStore(rootReducer, initialState, composedEnhancers)
 
     sagaMiddleware.run(rootSaga)
 
