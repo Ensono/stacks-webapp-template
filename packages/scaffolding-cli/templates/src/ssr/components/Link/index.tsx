6f9b6adb6884c63ef59ca6dea5b9a2d4e0842c5d
Naren Bellala <naren.bellala@amido.com>
2020-06-01 11:18:59 +0100

    fix: e2e tests

--- packages/scaffolding-cli/templates/src/ssr/components/Link/index.tsx
+++ packages/scaffolding-cli/templates/src/ssr/components/Link/index.tsx
@@ -1,8 +1,12 @@
 import React from "react"
+import getConfig from "next/config"
 import Link from "next/link"
+
+const {publicRuntimeConfig} = getConfig()
+export const {APP_BASE_PATH} = publicRuntimeConfig
 
 export const PrefixedLink: React.FC<Link["props"]> = ({
     href,
     as = href,
     ...props
-}) => <Link href={href} as={`${process.env.APP_BASE_PATH}${as}`} {...props} />
+}) => <Link href={href} as={`${APP_BASE_PATH}${as}`} {...props} />
