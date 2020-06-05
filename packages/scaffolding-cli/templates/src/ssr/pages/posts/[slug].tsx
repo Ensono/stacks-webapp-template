32941f38d9bdbb4ce67149999ed944e4a0301f2c
Naren Bellala <naren.bellala@amido.com>
2020-06-05 16:27:28 +0100

    fix: reverted change back

--- packages/scaffolding-cli/templates/src/ssr/pages/posts/[slug].tsx
+++ packages/scaffolding-cli/templates/src/ssr/pages/posts/[slug].tsx
@@ -48,10 +48,7 @@ export async function getStaticProps({params, preview = false}) {
 export async function getStaticPaths() {
     const allPosts = await getAllPostsWithSlug()
     return {
-        paths:
-            allPosts?.map(
-                ({slug}) => `${process.env.APP_BASE_PATH}/posts/${slug}`,
-            ) ?? [],
+        paths: allPosts?.map(({slug}) => `/posts/${slug}`) ?? [],
         fallback: true,
     }
 }
