a3e8d232976cfc5ba724b47bcfabdacfd34315ab
Naren Bellala <naren.bellala@amido.com>
2020-06-19 17:03:00 +0100

    chore: removed unused env vars

--- packages/scaffolding-cli/templates/src/ssr/lib/contentful-api.ts
+++ packages/scaffolding-cli/templates/src/ssr/lib/contentful-api.ts
@@ -5,14 +5,6 @@ const client = createClient({
     accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
 })
 
-const previewClient = createClient({
-    space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
-    accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_PREVIEW_ACCESS_TOKEN,
-    host: "preview.contentful.com",
-})
-
-const getClient = preview => (preview ? previewClient : client)
-
 function parseAuthor({fields}) {
     return {
         name: fields.name,
@@ -44,7 +36,7 @@ export async function getLanguages() {
 }
 
 export async function getPreviewPostBySlug(slug) {
-    const entries = await getClient(false).getEntries({
+    const entries = await client.getEntries({
         content_type: "post",
         limit: 1,
         "fields.slug[in]": slug,
@@ -63,7 +55,7 @@ export async function getAllPostsWithSlug() {
 }
 
 export async function getAllPostsForHome(preview) {
-    const entries = await getClient(preview).getEntries({
+    const entries = await client.getEntries({
         content_type: "post",
         order: "-fields.date",
     })
