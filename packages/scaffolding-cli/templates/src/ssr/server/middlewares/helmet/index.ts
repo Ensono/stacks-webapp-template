daa9cde9312ed5cbd666970e193857c0f37ba980
estherthetesteramido <esther.lloyd@amido.com>
2020-06-26 16:44:20 +0100

    fix: security headers default helmet

--- packages/scaffolding-cli/templates/src/ssr/server/middlewares/helmet/index.ts
+++ packages/scaffolding-cli/templates/src/ssr/server/middlewares/helmet/index.ts
@@ -1,8 +1,16 @@
 import Helmet from 'helmet'
 
 const helmetGuard = Helmet({
-  frameguard: false,
+  frameguard: {
+    action: 'same-origin'  
+  },
   xssFilter: true,
-  hidePoweredBy: true
+  hidePoweredBy: true,
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc: ["* 'unsafe-inline 'unsafe-eval'"]
+    }
+  }
 })
+
 export default helmetGuard
