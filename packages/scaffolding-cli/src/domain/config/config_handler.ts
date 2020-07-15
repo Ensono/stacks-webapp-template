164632c81ab5c57238071a8be11c1176b6abed59
Jack Blower <Jack@elvenspellmaker.co.uk>
2020-07-15 17:41:17 +0100

    fix: more lint

--- packages/scaffolding-cli/src/domain/config/config_handler.ts
+++ packages/scaffolding-cli/src/domain/config/config_handler.ts
@@ -3,10 +3,10 @@ import fs from "fs";
 
 let mergedConfig = {};
 
-glob("file_maps/*.config.json", function(error: Error | null, files: string[]) {
+glob("file_maps/*.config.json", (error: Error | null, files: string[]) => {
     files.forEach((file: string) => {
-        fs.readFile(file, 'utf8', function(err: NodeJS.ErrnoException | null, contents: string) {
-            let json = JSON.parse(contents);
+        fs.readFile(file, 'utf8', (err: NodeJS.ErrnoException | null, contents: string) => {
+            const json = JSON.parse(contents);
 
             mergedConfig = {
                 ...mergedConfig,
