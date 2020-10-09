7d90229cdb2a791a9146a0a62f90ccd7bbfb2220
Saul France <saul.france@amido.com>
2020-10-09 15:25:49 +0100

    fix: replace pipeline variables using cli

--- packages/scaffolding-cli/src/domain/config/worker_maps/ssr_aks_tfs.ts
+++ packages/scaffolding-cli/src/domain/config/worker_maps/ssr_aks_tfs.ts
@@ -35,15 +35,18 @@ export const inFiles = ({
             files: ["**/app-pipeline.yml"],
             values: {
                 "domain: node": `domain: ${businessObj.domain}`,
-                "component: node": `domain: ${businessObj?.component}`,
+                "component: \\$\\(pipeline_variable_component\\)": `component: webapp`,
                 "src/ssr": "src",
                 "nonprod.amidostacks.com": `${networkObj.baseDomain}`,
+                "docker_image_name: \\$\\(component\\)": "docker_image_name: $(self_generic_name)",
                 "amido-stacks-webapp": "REPLACE_ME_FOR_APP_SPECIFIC_LIBRARY_VARIABLES",
                 "tf_state_key: stacks-webapp": `tf_state_key: %REPLACE_ME_FOR_STATE_KEY_FOR_MY_APP%`,
                 "deploy/azure/app/kube": "deploy/azure/app",
-                "terraform_state_workspace: dev": "terraform_state_workspace: %REPLACE_ME_FOR_WORKSPACE_NAME_IN_EACH_STAGE%",
+                "terraform_state_workspace: dev-\\$\\(component\\)": "terraform_state_workspace: %REPLACE_ME_FOR_WORKSPACE_NAME_IN_EACH_STAGE%",
                 "docker_container_registry_name: amidostacksnonprodeuncore": "docker_container_registry_name: REPLACE_ME_FOR_CONTAINER_REGISTRY",
-                "amido-stacks-nonprod-eun-core": "REPLACE_ME_FOR_CLOUD_RESOURCE_NAME"
+                "amido-stacks-nonprod-eun-core": "REPLACE_ME_FOR_CLOUD_RESOURCE_NAME",
+                "dev-\\$\\(component\\)": "dev-webapp",
+                "\\$\\(pipeline_variable_api\\)": "api"
             }
         }
     ]
