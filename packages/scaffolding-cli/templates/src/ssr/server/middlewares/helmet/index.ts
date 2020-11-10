f78cd1d6078be5cbfbeb2370c74a9bc2499f9036
Saul France <saul.france@amido.com>
2020-11-10 16:46:05 +0000

    fix: set x-xss-protection header

--- packages/scaffolding-cli/templates/src/ssr/server/middlewares/helmet/index.ts
+++ packages/scaffolding-cli/templates/src/ssr/server/middlewares/helmet/index.ts
@@ -5,6 +5,7 @@ const helmetGuard = Helmet({
   frameguard: {
     action: 'same-origin'
   },
+  xssFilter: false
 //   contentSecurityPolicy: {
 //     directives: {
 //       defaultSrc: ["* 'unsafe-inline 'unsafe-eval'"]
@@ -15,6 +16,11 @@ const helmetGuard = Helmet({
 // https://github.com/helmetjs/helmet/wiki/How-to-set-a-custom-X%E2%80%93Powered%E2%80%93By-header
 export function removeXPoweredByHeader(req: Request, res: Response, next: NextFunction) {
     res.removeHeader("X-Powered-By")
+    return next()
+}
+
+export function setXSSProtectionHeader(req: Request, res: Response, next: NextFunction) {
+    res.setHeader('X-XSS-Protection', '1; mode=block');
     return next()
 }
 
